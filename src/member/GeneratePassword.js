0912ee5663e4a666020c4afd2b5b096a696314b7
Suhyun Kim <suhyunroom@naver.com>
2024-06-01 05:07:13 +0900

    search

--- src/member/GeneratePassword.js
+++ src/member/GeneratePassword.js
@@ -12,10 +12,6 @@ function GeneratePassword() {
             <div id="result">
                 임시 비밀번호가 발급되었습니다.
             </div>
-            <div id="resultId">
-                <div>새로운 비밀번호</div>
-                <div>asdf1234 {/*비밀번호 발급*/}</div>
-            </div>
             <div className="btn">
                 <Link to="/login">확인</Link>
             </div>
--- src/member/ResultSearch.js
+++ src/member/ResultSearch.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import './Style.css'; // CSS 파일을 import
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import logoImage from '../img/semohan-logo.png';
 import toMain from "../img/toMain.png";
 import searchBtn from "../img/search.png";
+import searchImage from "../img/search.png";
+import example from "../img/buffetjpg.jpg";
+import bookmarkImage from "../img/bookmark-white.png";
 
 function ResultSearch() {
+    const navigate = useNavigate();
     return (
-        <div id="body">
+        <div id="newBody">
             <header>
                 <img src={logoImage} alt="logo"/>
             </header>
             <div id="searchBar">
-                <img src={toMain} alt="toMain"/>
+                <img src={toMain} alt="toMain" onClick={() => navigate('/mainNoLogin')}/>
                 <input type="text"
                        name="search"
                        className="search"
-                       // value={/*검색어*/}
+                       placeholder="지역, 음식 또는 식당 입력"
                 />
-                <img src={searchBtn} alt="searchBtn"/>
+                <img className="headerImg" src={searchImage} onClick={() => navigate('/resultSearch')} alt="search"/>
             </div>
-            <div>
 
+            <div id="main_noLogin">
+                <div className="image-grid">
+                    {/*식당 수만큼*/}
+                    <div className="image-container">
+                        <img className="resImg" src={example/*식당사진*/} alt="search"/>
+                        <img className="bookmark-image" src={bookmarkImage} onClick={{/*클릭마다 사진 바뀜, 스크랩 등록+취소*/}}/>
+                        <span className="image-caption">뷔페1</span>
+                    </div>
+                    {/*<div className="image-container">*/}
+                    {/*    <img className="resImg" src={example/*식당사진*!/ alt="search"/>*/}
+                    {/*    <img className="bookmark-image" src={bookmarkImage} onClick={/!*클릭마다 사진 바뀜, 스크랩 등록+취소*!/}/>*/}
+                    {/*    <span className="image-caption">뷔페1</span>*/}
+                    {/*</div>*/}
+                    {/*<div className="image-container">*/}
+                    {/*    <img className="resImg" src={example/*식당사진*!/ alt="search"/>*/}
+                    {/*    <img className="bookmark-image" src={bookmarkImage} onClick={/!*클릭마다 사진 바뀜, 스크랩 등록+취소*!/}/>*/}
+                    {/*    <span className="image-caption">뷔페1</span>*/}
+                    {/*</div><div className="image-container">*/}
+                    {/*    <img className="resImg" src={example/*식당사진*!/ alt="search"/>*/}
+                    {/*    <img className="bookmark-image" src={bookmarkImage} onClick={/!*클릭마다 사진 바뀜, 스크랩 등록+취소*!/}/>*/}
+                    {/*    <span className="image-caption">뷔페1</span>*/}
+                    {/*</div><div className="image-container">*/}
+                    {/*    <img className="resImg" src={example/*식당사진*!/ alt="search"/>*/}
+                    {/*    <img className="bookmark-image" src={bookmarkImage} onClick={/!*클릭마다 사진 바뀜, 스크랩 등록+취소*!/}/>*/}
+                    {/*    <span className="image-caption">뷔페1</span>*/}
+                    {/*</div>*/}
+                </div>
             </div>
         </div>
     );
