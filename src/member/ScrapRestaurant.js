06fe6ee5adf8188dabd7cb753f7826484dc0851c
Suhyun Kim <suhyunroom@naver.com>
2024-06-09 12:27:28 +0900

    scrapRestaurant ㅕupdate btn

--- src/member/ScrapRestaurant.js
+++ src/member/ScrapRestaurant.js
@@ -7,19 +7,42 @@ import searchImage from "../img/search.png";
 import example from "../img/buffetjpg.jpg";
 import pin from "../img/pin.png";
 import noPin from "../img/noPin.png";
+import noScrap from "../img/bookmark-white.png";
+import scrap from "../img/bookmark-black.png";
 
 function ScrapRestaurant() {
     const navigate = useNavigate();
     const [pinImages, setPinImages] = useState([noPin, noPin]); // 각 핀의 스크랩 이미지를 배열로 관리
+    const [scrapImages, setScrapImages] = useState([noScrap, noScrap]); // 각 핀의 스크랩 이미지를 배열로 관리
     const [pinImage, setPinImage] = useState(pin);
+    const [scrapImage, setScrapImage] = useState(scrap);
+
+    const handelNoScrap = () => {
+        setScrapImage((prevSrc) => (prevSrc === scrap ? noScrap : scrap));
+    }
+
+    const [imageStates, setImageStates] = useState([
+        {pin: noPin, scrap: noScrap},
+        {pin: noPin, scrap: noScrap}
+    ]);
 
     const handleScrap = (index) => {
-        setPinImages(prevImages =>
-            prevImages.map((img, i) => (i === index ? (img === noPin ? pin : noPin) : img))
+        setImageStates(prevStates =>
+            prevStates.map((state, i) => (
+                i === index ? {...state, scrap: state.scrap === noScrap ? scrap : noScrap} : state
+            ))
         );
     }
 
-    const handlePin = () => {
+    const handlePin = (index) => {
+        setImageStates(prevStates =>
+            prevStates.map((state, i) => (
+                i === index ? {...state, pin: state.pin === noPin ? pin : noPin} : state
+            ))
+        );
+    }
+
+    const handleNoPin = () => {
         setPinImage((prevSrc) => (prevSrc === pin ? noPin : pin));
     }
 
@@ -37,7 +60,8 @@ function ScrapRestaurant() {
                 <div className="image-grid">
                     <div className="image-container">
                         <img className="resImg" src={example/*식당사진*/} alt="search"/>
-                        <img className="bookmark-image" src={pinImage} onClick={handlePin}/>
+                        <img className="bookmark-image1" src={scrapImage} onClick={handelNoScrap}/>
+                        <img className="bookmark-image2" src={pinImage} onClick={handleNoPin}/>
                         <span className="image-caption" onClick={() => navigate('/detailRestaurant')}>뷔페1</span>
                     </div>
                 </div>
@@ -48,19 +72,27 @@ function ScrapRestaurant() {
 
                 <h4>내가 스크랩한 식당</h4>
                 <div className="image-grid">
-                    {pinImages.map((img, index) => (
+                    {imageStates.map((state, index) => (
                         <div className="image-container" key={index}>
-                            <img className="resImg" src={example/*식당사진*/} alt="search"/>
+                            <img className="resImg" src={example} alt="search"/>
                             <img
-                                className="bookmark-image"
-                                src={img}
+                                className="bookmark-image1"
+                                src={state.scrap}
                                 onClick={() => handleScrap(index)}
-                                alt="bookmark"
+                                alt="scrap"
                             />
-                            <span className="image-caption" onClick={() => navigate('/detailRestaurant')}>뷔페{index + 1}</span>
+                            <img
+                                className="bookmark-image2"
+                                src={state.pin}
+                                onClick={() => handlePin(index)}
+                                alt="pin"
+                            />
+                            <span className="image-caption"
+                                  onClick={() => navigate('/detailRestaurant')}>뷔페{index + 1}</span>
                         </div>
                     ))}
                 </div>
+                <button className='submit gray' >저장</button>
 
             </div>
         </div>
