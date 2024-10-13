3366a62e37ab38cfb395289014eefec1be8dfd53
Yujung Choi <yujung5548@naver.com>
2024-10-14 02:46:28 +0900

    Fix: "Scrap 기능 수정"
    Main, ResultSearch, DetailRestaurant 페이지에서의 스크랩 기능 수정

--- src/member/ResultSearch.js
+++ src/member/ResultSearch.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
 import './Style.css'; // CSS 파일을 import
 import {Link, useLocation, useNavigate} from 'react-router-dom';
 import logoImage from '../img/semohan-logo.png';
@@ -7,7 +7,7 @@ import searchBtn from "../img/search.png";
 import searchImage from "../img/search.png";
 import example from "../img/buffetjpg.jpg";
 import noScrap from "../img/bookmark-white.png";
-import scrap from "../img/bookmark.png";
+import scrap from "../img/bookmark-black.png";
 import bookmarkImage from "../img/bookmark-white.png";
 import axios from "axios";
 import qs from "qs";
@@ -16,17 +16,28 @@ import ScrapContext from './ScrapContext'; // ScrapContext import
 
 function ResultSearch() {
     const navigate = useNavigate();
-    const { scrapStatus, updateScrapStatus } = useContext(ScrapContext); // ScrapContext 사용
+    // const { scrapStatus, updateScrapStatus } = useContext(ScrapContext); // ScrapContext 사용
 
     const location = useLocation();
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
-    const [scrapImage, setScrapImage] = useState(noScrap);
-    const handelScrap = () => {
-        setScrapImage((prevSrc) => (prevSrc === noScrap ? scrap : noScrap));
-    }
+    // const [scrapImage, setScrapImage] = useState(noScrap);
+
     const [searchType, setSearchType] = useState(''); // 검색 타입 추가
 
+    const [scrapImages, setScrapImages] = useState([noScrap, noScrap]); // 각 핀의 스크랩 이미지를 배열로 관리
+
+    const [loggedIn, setLoggedIn] = useState(false); // 로그인 여부 상태
+    const [pinnedRestaurant, setPinnedRestaurant] = useState(null); // 핀한 식당 목록 상태
+    const [pinnedRestaurantName, setPinnedRestaurantName] = useState(''); // 핀한 식당 목록 상태
+
+    const [restaurantName, setRestaurantName] = useState(''); // 핀된 식당 이름 상태
+
+    const [scrappedRestaurants, setScrappedRestaurants] = useState([]);
+    const [scrapImage, setScrapImage] = useState(scrap);
+    const [restaurants, setRestaurants] = useState([]);
+
+    const { scrapStatus, setScrapStatus, setContextRestaurants  } = useContext(ScrapContext);
     useEffect(() => {
         if (location.state && location.state.results) {
             setSearchResults(location.state.results);
@@ -34,6 +45,95 @@ function ResultSearch() {
             setSearchTerm(location.state.searchTerm);
         }
     }, [location.state]);
+
+    const fetchScrapStatus = useCallback(async () => {
+        try {
+            const response = await axios.get('/restaurant/scrap-pin', { withCredentials: true });
+            const scrapList = response.data.scrappedRestaurnats || [];
+
+            setSearchResults(prevSearchResults =>
+                prevSearchResults.map(restaurant => ({
+                    ...restaurant,
+                    isScrapped: scrapList.some(scrap => scrap.id === restaurant.id)
+                }))
+            );
+        } catch (error) {
+            console.error("There was an error fetching scrap status!", error);
+        }
+    }, []);
+
+
+    const fetchRestaurants = useCallback(async (location) => {
+        try {
+            const response = await axios.get(`/restaurant/nearby`, {
+                withCredentials: true,
+                params: { location: location }
+            });
+            const fetchedRestaurants = response.data;
+            // console.log(fetchedRestaurants);
+            // 각 식당의 스크랩 상태를 확인하여 설정
+            const updatedRestaurants = fetchedRestaurants.map((restaurant) => ({
+                ...restaurant,
+                isScrapped: loggedIn ? scrapStatus.find(status => status.id === restaurant.id)?.isScrapped || false : false
+            }));
+
+            // setRestaurants(updatedRestaurants); // 업데이트된 식당 리스트 설정
+            setRestaurants(fetchedRestaurants); // 검색된 식당 리스트 설정
+
+            fetchScrapStatus();
+        } catch (error) {
+            console.error("There was an error fetching the restaurant data!", error);
+        }
+    }, [fetchScrapStatus]);
+
+    const checkLoginStatus = useCallback(() => {
+        axios.get('/member/info', { withCredentials: true })
+            .then(response => {
+                if (response.data) {
+                    setLoggedIn(true);
+                    // fetchPinnedRestaurantMenu ();
+                    fetchScrapStatus(); // 로그인 시 스크랩 상태 가져오기
+
+                }
+            }).catch(error => {
+            console.error("There was an error checking login status!", error);
+        });
+    }, [loggedIn]);
+
+    useEffect(() => {
+        if (loggedIn && searchResults.length > 0) {
+            // fetchPinnedRestaurantMenu(); // 핀한 식당 메뉴 가져오기
+            // fetchPinnedRestaurantName(); // 핀한 식당 이름 가져오기
+            fetchRestaurants(); // 로그인 시 레스토랑 불러오기
+        }
+        // }, [loggedIn, restaurants]);
+    }, [loggedIn, searchResults, fetchScrapStatus]);
+
+    useEffect(() => {
+        axios.get('/restaurant/scrap-pin')
+            .then(response => {
+                console.log("스크랩의내용")
+                console.log(response.data)
+                const data = response.data;
+
+                const pinnedRestaurant = data.pinnedRestaurnat; // 백엔드의 오타 처리
+
+                const scrappedRestaurants = Array.isArray(data.scrappedRestaurnats) ? data.scrappedRestaurnats : [];
+
+                // setPinnedRestaurant(data.pinnedRestaurant);
+                setPinnedRestaurant(pinnedRestaurant);
+                setScrappedRestaurants(scrappedRestaurants.map(restaurant => ({
+                    ...restaurant,
+                    scrapped: true,
+                    pinned: pinnedRestaurant && pinnedRestaurant.id === restaurant.id // 현재 핀한 식당과 비교
+                    // pinned: data.pinnedRestaurant && data.pinnedRestaurant.id === restaurant.id
+                })));
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
 
     const handleSearchChange = (e) => {
         setSearchTerm(e.target.value);
@@ -82,11 +182,55 @@ function ResultSearch() {
         navigate(`/detailRestaurant/${restaurantId}`);
     };
 
-    const handleScrapToggle = (restaurantId, index) => {
-        const isScrapped = scrapStatus[index]; // 현재 스크랩 상태 가져오기
-        updateScrapStatus(restaurantId, !isScrapped); // 스크랩 상태 업데이트
+    const handleScrap = (restaurantId, isScrapped) => {
+        const apiUrl = isScrapped
+            ? `/restaurant/delete-scrap/${restaurantId}`
+            : `/restaurant/scrap/${restaurantId}`;
+
+        axios.post(apiUrl, {}, { withCredentials: true })
+            .then(() => {
+                setSearchResults(prevSearchResults =>
+                    prevSearchResults.map(restaurant =>
+                        restaurant.id === restaurantId
+                            ? { ...restaurant, isScrapped: !isScrapped } // 스크랩 상태 업데이트
+                            : restaurant
+                    )
+                );
+
+                alert(isScrapped ? '스크랩 해제되었습니다.' : '스크랩되었습니다.');
+            })
+            .catch(error => {
+                console.error(isScrapped ? "Error removing scrap!" : "Error adding scrap!", error);
+            });
     };
 
+
+    const updatePinnedScrappedRestaurants = (scrappedRestaurantIdList, pinnedRestaurantId) => {
+        const payload = {
+            scrappedRestaurantIdList,
+            pinnedRestaurantId,
+        };
+
+        axios.post('/restaurant/scrap-pin/update', payload, { withCredentials: true })
+            .then(response => {
+                if (response.data) {
+                    console.log("핀 및 스크랩 정보가 성공적으로 업데이트되었습니다.");
+                }
+            })
+            .catch(error => {
+                console.error("핀 및 스크랩 정보 업데이트 중 오류가 발생했습니다!", error);
+            });
+    };
+
+    useEffect(() => {
+        // fetchRestaurants(); // Initially fetch restaurants
+        checkLoginStatus(); // Check login status on component mount
+    }, [checkLoginStatus]);
+
+    useEffect(() => {
+        // fetchRestaurants(); // Initially fetch restaurants
+        fetchScrapStatus(); // Check login status on component mount
+    }, []);
     return (
         <div id="newBody">
 
@@ -115,16 +259,17 @@ function ResultSearch() {
                     {searchResults.length > 0 ? (
                             searchResults.map((restaurant, index) => (
 
-                    <div className="image-container" key={index}>
-                        {searchResults.data}
-                        <img className="resImg" src={restaurant.s3Url} alt="search"  onClick={() => handleImageClick(restaurant.id)}/>
-                        <img className="bookmark-image2" src={bookmarkImage}
-                             onClick={() => handleScrapToggle(restaurant.id, index)}
-                             alt="스크랩"/>
-                        <span className="image-caption">{restaurant.name}</span>
-                    </div>
-                        ))
-                     ) : (
+                                <div className="image-container" key={index}>
+                                    {searchResults.data}
+                                    <img className="resImg" src={restaurant.s3Url} alt="search"
+                                         onClick={() => handleImageClick(restaurant.id)}/>
+                                    <img className="bookmark-image2" src={restaurant.isScrapped ? scrap : noScrap}
+                                         onClick={() => handleScrap(restaurant.id, restaurant.isScrapped)}
+                                         alt="bookmark"/>
+                                    <span className="image-caption">{restaurant.name}</span>
+                                </div>
+                            ))
+                    ) : (
                         <div>검색 결과가 없습니다.</div>
                     )}
 
--- src/member/ScrapContext.js
+++ src/member/ScrapContext.js
@@ -12,9 +12,15 @@ export const ScrapProvider = ({ children }) => {
     // 스크랩 상태를 서버에서 가져오는 함수
     useEffect(() => {
         const fetchScrapStatus = async () => {
+
+            if (restaurants.length === 0) {
+                console.log("There isn't restaurant's info")
+                return;
+            } // 레스토랑이 없으면 스크랩 상태를 가져오지 않음
+
             try {
                 const response = await axios.get('/restaurant/scrap-pin', { withCredentials: true });
-                const scrapList = response.data.scrappedRestaurants || [];
+                const scrapList = response.data.scrappedRestaurnats || [];
 
                 // 받아온 데이터와 현재 레스토랑 리스트를 비교하여 스크랩 상태 업데이트
                 const updatedScrapStatus = restaurants.map(restaurant =>
@@ -24,27 +30,36 @@ export const ScrapProvider = ({ children }) => {
                 setScrapStatus(updatedScrapStatus); // 스크랩 상태 설정
             } catch (error) {
                 console.error("There was an error fetching scrap status!", error);
+
+                // 오류 시 기본값 설정
+                setScrapStatus(Array(restaurants.length).fill(false));
             }
         };
 
         fetchScrapStatus();
-    }, [restaurants]);
+    }, []);
 
     // 스크랩 상태 업데이트 함수
     const updateScrapStatus = (restaurantId, isScrapped) => {
-        setScrapStatus(prevStatus =>
-            prevStatus.map((status, index) => {
-                if (restaurants[index].id === restaurantId) {
-                    return isScrapped;
+        // setScrapStatus(prevStatus =>
+        //     prevStatus.map((status, index) => {
+        //         if (restaurants[index].id === restaurantId) {
+        //             return isScrapped;
+        //
+        //         }
+        //         return prevStatus[index];
+        //     })
+        // );
 
-                }
-                return prevStatus[index];
-            })
+        setScrapStatus(prevStatus =>
+            prevStatus.map((status, index) =>
+                restaurants[index].id === restaurantId ? isScrapped : status
+            )
         );
     };
 
     return (
-        <ScrapContext.Provider value={{ scrapStatus, updateScrapStatus, setRestaurants }}>
+        <ScrapContext.Provider value={{ scrapStatus, setScrapStatus, updateScrapStatus, setRestaurants }}>
             {children}
         </ScrapContext.Provider>
     );
